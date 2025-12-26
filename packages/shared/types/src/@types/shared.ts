741ed1053bfbd8d7ff4418305c581e1c506cb269
gratisvictory <gratisvictory@gmail.com>
2025-12-27 02:29:15 +0400

    📦 refactor(types): convert to type-only package and add prefix utilities

--- packages/shared/types/src/@types/shared.ts
+++ packages/shared/types/src/@types/shared.ts
@@ -1,3 +1,6 @@
-type Awaitable<T> = T | Promise<T>;
+type TAwaitable<T> = T | Promise<T>;
+type TMaybeUndefined<T> = T | undefined;
+type TMaybeNull<T> = T | null;
+type TMaybeBoolean<T> = T | boolean;
 
-export type { Awaitable };
+export type { TAwaitable, TMaybeUndefined, TMaybeNull, TMaybeBoolean };
